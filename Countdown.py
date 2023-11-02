144b4075760ba69c20d68916bd6638110c45c109
Steve Shay <fake@notmyemail.com>
2023-11-02 16:23:32 -0400

    add Countdown.py with game framework

---
+++ Countdown.py
@@ -0,0 +1,29 @@
+# Game based off of the British gameshow, Countdown. 
+# With inspiration coming from the '8 out of 10 cats' variation.
+
+# **** Rules ****
+# see: https://wiki.apterous.org/Main_Page
+
+# **** Gameplay ****
+# Two Letter Rounds, Two Numbers Rounds, One final Conundrum
+# I believe it's L, N, L, N, C
+
+
+# **** Numbers Round ****
+# Choose 6 numbers to include a max of 4 large and a randomized target
+# Large: 25, 50, 75 100
+# Small: 1, 2, 3 ,4 5, 6, 7, 8, 9
+# Target: Random between 100 and 1000 (my guess - actual rule?) 
+# Any Contenstant / Team with the valid answer gets points (how many?)
+
+
+# **** Letters Round ****
+# Choose 9 letters to include at least 3 vowels and 4 consonents
+# Possible combinations: 3v 6c, 4v 5c, 5v 4c
+# Contenstant / Team with the longest word gets 1 point per letter
+# Nine letters words are worth double points, or 18
+
+
+# **** Conundrum Round ****
+# 2 or 3 words with a total of 9 letters and a valid anagram
+# Generally 10 points to the solving team, although variations exist
